935879937e0a0452efa566b43e31e9c76853adb5
Priyankar Kumar <priyankar.kumar98@gmail.com>
2023-05-28 10:21:49 +0530

    Error handling

--- pages/api/openai.ts
+++ pages/api/openai.ts
@@ -18,33 +18,37 @@ export default async function handler(
     console.log(req.query);
 
     const history = JSON.parse((req.query.history as string) ?? "[]");
-
-    fetchEventSource('https://api.openai.com/v1/chat/completions', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
-        },
-        body: JSON.stringify({
-            messages: [...basePrompt, ...history],
-            "model": "gpt-4-0314",
-            stream: true,
-            max_tokens: 2800,
-            temperature: 0.3
-        }),
-        onmessage: (event: EventSourceMessage) => {
-            //console.log(event);
-            res.write(`data: ${event.data}\n\n`);
-            if (event.data === "[DONE]") {
+    try {
+        fetchEventSource('https://api.openai.com/v1/chat/completions', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+            },
+            body: JSON.stringify({
+                messages: [...basePrompt, ...history],
+                "model": "gpt-4-0314",
+                stream: true,
+                max_tokens: 2800,
+                temperature: 0.3
+            }),
+            onmessage: (event: EventSourceMessage) => {
+                //console.log(event);
+                res.write(`data: ${event.data}\n\n`);
+                if (event.data === "[DONE]") {
+                    res.end();
+                }
+            },
+            onerror: (error: Error) => {
+                console.log(error);
+                res.end();
+            },
+            onclose: () => {
                 res.end();
             }
-        },
-        onerror: (error: Error) => {
-            console.log(error);
-            res.end();
-        },
-        onclose: () => {
-            res.end();
-        }
-    });
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).end();
+    }
 }

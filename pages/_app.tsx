a45a8117bc4a802d6b56aaa000edf4adada7bfc7
Priyankar Kumar <priyankar.kumar98@gmail.com>
2023-05-28 09:48:01 +0530

    Make it static bundle

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,17 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 
+function SafeHydrate({ children }: any) {
+  return (
+    <div suppressHydrationWarning>
+      {typeof window === 'undefined' ? null : children}
+    </div>
+  )
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return <ChakraProvider>
-    <Component {...pageProps} />
-  </ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+ 
 }

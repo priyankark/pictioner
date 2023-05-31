42e8c3c2e681bc1dd9a3a665b826f88b0ef03bd2
Priyankar Kumar <priyankar.kumar98@gmail.com>
2023-05-31 14:50:30 +0530

    Added Product Hunt badge

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Box, Link, Text, VStack } from '@chakra-ui/react';
+import { Box, Flex, Link, Text, VStack } from '@chakra-ui/react';
 
 const Footer = () => {
     return (
@@ -17,13 +17,22 @@ const Footer = () => {
                         priyankar.me
                     </Link>
                 </Text>
-                <Link href="https://www.buymeacoffee.com/priyankark" isExternal>
-                    <img
-                        src="https://img.buymeacoffee.com/button-api/?text=Buy%20me%20a%20coffee&emoji=&slug=priyankark&button_colour=FFDD00&font_colour=000000&font_family=Lato&outline_colour=000000&coffee_colour=ffffff"
-                        alt="Buy Me a Coffee"
-                        style={{ height: 'auto', width: 'auto' }}
-                    />
-                </Link>
+                <Flex direction="row" align="center" justify="center" gap={2}>
+                    <Box>
+                        <Link href="https://www.producthunt.com/posts/pictioner?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-pictioner" isExternal>
+                            <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=397158&theme=light" alt="Pictioner - Test&#0032;your&#0032;wits&#0032;against&#0032;GPT&#0045;4&#0032;in&#0032;a&#0032;fun&#0032;art&#0032;guessing&#0032;game | Product Hunt" style={{ width: "250px", height: "54px" }} width="250" height="54" />
+                        </Link>
+                    </Box>
+                    <Box>
+                        <Link href="https://www.buymeacoffee.com/priyankark" isExternal>
+                            <img
+                                src="https://img.buymeacoffee.com/button-api/?text=Buy%20me%20a%20coffee&emoji=&slug=priyankark&button_colour=FFDD00&font_colour=000000&font_family=Lato&outline_colour=000000&coffee_colour=ffffff"
+                                alt="Buy Me a Coffee"
+                                style={{ height: 'auto', width: 'auto' }}
+                            />
+                        </Link>
+                    </Box>
+                </Flex>
             </VStack>
         </Box>
     );
